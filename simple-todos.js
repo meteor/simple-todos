4e6f9494c895eff43fe26167a7b803a24c35854e
Sashko Stubailo <sashko@mit.edu>
2014-10-22 11:17:14 -0700

    Make it possible to check off and delete tasks

--- simple-todos.js
+++ simple-todos.js
@@ -26,4 +26,14 @@ if (Meteor.isClient) {
       return false;
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
