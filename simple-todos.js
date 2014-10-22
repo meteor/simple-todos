ff9bdbfce257032b4499b4f87bd477a8bc65af3b
Sashko Stubailo <sashko@mit.edu>
2014-10-22 11:12:43 -0700

    Replace the static array with a Collection

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,10 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
