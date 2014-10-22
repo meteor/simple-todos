1288b34ca3042515a0dd6d8e9e606bfbddbda7af
Sashko Stubailo <sashko@mit.edu>
2014-10-22 11:19:27 -0700

    Add an option to hide completed tasks, and show incomplete task count

--- simple-todos.js
+++ simple-todos.js
@@ -4,8 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      // Show newest tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -24,6 +35,9 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+    "click .hide-completed": function () {
+      Session.set("hideCompleted", ! Session.get("hideCompleted"));
     }
   });
 
