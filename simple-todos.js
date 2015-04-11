4bc4ab7768994582f377acafd650f56adea89d5f
Geoffrey Litt <glitt@panoramaed.com>
2015-04-11 18:04:38 +0400

    Use preventDefault to prevent form submit behavior
    The return false technique does not work if an error is introduced
    in the event handler due to a user modifying the tutorial, or
    copying it in their own project.

--- simple-todos.js
+++ simple-todos.js
@@ -24,6 +24,9 @@ if (Meteor.isClient) {
 
   Template.body.events({
     "submit .new-task": function (event) {
+      // Prevent default form submit
+      event.preventDefault();
+
       // This function is called when the new task form is submitted
       var text = event.target.text.value;
 
@@ -31,9 +34,6 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
-
-      // Prevent default form submit
-      return false;
     },
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
