aa0357a3c29f7fdedfbb7ff2b109e990831bb399
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:30:20 -0700

    Step 9.2: Define some methods

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,34 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
