d7e04b692a361153918f4a88548dfdc1c0220367
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:26:17 -0700

    Step 8.4: Add incompleteCount helper to body

--- simple-todos.js
+++ simple-todos.js
@@ -14,6 +14,9 @@ if (Meteor.isClient) {
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
