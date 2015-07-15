686b3ad37564f5cf4b2caa0491304ec660ccf887
Brian Mulhall <brian.e.mulhall@gmail>
2015-07-14 19:31:24 -0700

    Step 9.3: Replace insert with addTask method

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -36,12 +36,7 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
