8422a894f5acb8776657e6ec366a9cb174dfbd33
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:26:17 -0700

    Step 7.6: Add incompleteCount helper to body

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -20,6 +20,9 @@ Template.body.helpers({
     // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
 
 Template.body.events({
