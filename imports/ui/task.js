f0d911a3d2ae67b14ee3298715a67f3c4227e104
Brian Mulhall <brian.e.mulhall@gmail>
2015-07-14 19:31:46 -0700

    Step 9.4: Replace update and remove with methods

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,17 +1,14 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-
-import { Tasks } from '../api/tasks.js';
 
 import './task.html';
 
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   'click .delete'() {
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
   },
 });
