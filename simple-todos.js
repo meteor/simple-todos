fa380edb61cf1a07044f23afa76acb51b7afaf50
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:33:17 -0700

    Step 10.2: Add publish and subscribe

--- simple-todos.js
+++ simple-todos.js
@@ -1,7 +1,16 @@
 Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("tasks", function () {
+    return Tasks.find();
+  });
+}
+
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
