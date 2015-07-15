a06c2235fa31bb876edc2f62ba62310dee40ab6c
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:12:14 -0700

    Step 4.3: Show newest tasks at the top

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,8 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
