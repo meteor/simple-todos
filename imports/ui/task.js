652cdae86a86d1b0e4df67b0e8a1584a48f314f2
Brian Mulhall <brian.e.mulhall@gmail>
2015-07-14 19:34:47 -0700

    Step 10.6: Define helper to check ownership

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -3,6 +3,12 @@ import { Template } from 'meteor/templating';
 
 import './task.html';
 
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
