7a1ace1482bbdd5ce38aa5db28ecb20ce0e12863
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:11:45 -0700

    Step 4.2: Add event handler for form submit

--- simple-todos.js
+++ simple-todos.js
@@ -7,4 +7,23 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
 }
