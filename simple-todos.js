6682fe168d69eb11b7fe280b67ae87fc6c06425a
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:36:38 -0700

    Step 11.8: Only publish tasks the user is allowed to see

--- simple-todos.js
+++ simple-todos.js
@@ -2,8 +2,14 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isServer) {
   // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish("tasks", function () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 
