f3a0faebe7b53432e719148d109202a3179e207d
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:35:31 -0700

    Step 10.7: Define method to set tasks to private

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -38,4 +38,17 @@ Meteor.methods({
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  },
 });
