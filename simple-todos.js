383896358ebfb53c237a8611fc11a86aa71fbbf6
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:13:40 -0700

    Step 5.2: Add event handlers for Task buttons

--- simple-todos.js
+++ simple-todos.js
@@ -27,4 +27,16 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
