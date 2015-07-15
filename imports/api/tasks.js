e8a9282211d6f1bff187fd2d8d8abd7df62591c4
Sashko Stubailo <sashko@stubailo.com>
2015-07-14 19:37:36 -0700

    Step 10.10: Add some extra security to methods

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -36,11 +36,23 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
