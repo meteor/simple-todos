b818fb9487bd9b521f59586f0565563d268aa907
Minjie Xu <Minjie.Xu@ehi.com>
2015-10-24 21:40:28 -0500

    change default task sorting to ascending by create time

--- simple-todos.js
+++ simple-todos.js
@@ -21,10 +21,10 @@ if (Meteor.isClient) {
     tasks: function () {
       if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: 1}});
       } else {
         // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
+        return Tasks.find({}, {sort: {createdAt: 1}});
       }
     },
     hideCompleted: function () {
