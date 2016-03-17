9d2fe73799c42e09d090b3e04aa3cc670b57dbf1
Brian Mulhall <brian.e.mulhall@gmail>
2016-03-17 12:45:32 +1100

    Step 10.2: Add publication for tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,13 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
