1a33dc2c62700efa400e60ad561bbe03a8b03160
Tom Coleman <tom@thesnail.org>
2016-03-18 12:36:41 +1100

    Step 11.2: Add a scaffold for a method test

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      it('can delete owned task', () => {
+      });
+    });
+  });
+}
