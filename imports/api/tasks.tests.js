23156828acfc00fbcc682d6ab2212b18f85ff660
Tom Coleman <tom@thesnail.org>
2016-03-18 12:39:33 +1100

    Step 11.4: Added test to check delete method

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,6 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
 
 import { Tasks } from './tasks.js';
 
@@ -22,6 +23,18 @@ if (Meteor.isServer) {
       });
 
       it('can delete owned task', () => {
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        // Run the method with `this` set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
       });
     });
   });
