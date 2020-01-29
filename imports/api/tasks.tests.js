62658c207fbf9d209ece9493554208769992054d
Jan Küster <jkuester@uni-bremen.de>
2020-01-29 11:39:10 +0100

    update tests to reflect latest practices

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,7 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert } from 'chai';
 
 import { Tasks } from './tasks.js';
 
---
+++ tests/main.js
@@ -0,0 +1,1 @@
+import '../imports/api/tasks.tests';
