2d21f980a180ceeacd110b1b9d83c424629cc416
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-07 13:36:54 -0600

    Step 2.3: Add starter JS code

---
+++ imports/ui/body.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import './body.html';
+
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
