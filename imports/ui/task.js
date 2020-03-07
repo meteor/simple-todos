f6a75f177953db9cf34f31079d2230ae14d3d267
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-07 14:37:33 -0600

    Step 10.8: Add event handler to call the setPrivate metho

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -18,5 +18,8 @@ Template.task.events({
   },
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
+    },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
