bca010597f4af2b3dc01f32029661d3e4f5baf7a
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-07 13:52:19 -0600

    Step 7.3: Add state dictionary to the body

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,9 +1,14 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+  });
 
 Template.body.helpers({
     tasks() {
