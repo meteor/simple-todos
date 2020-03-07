60afe2159b407d19419c781921301b2b3bf1587e
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-07 13:53:36 -0600

    Step 7.5: Add helpers to body template

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -12,7 +12,12 @@ Template.body.onCreated(function bodyOnCreated() {
 
 Template.body.helpers({
     tasks() {
-        // Show newest tasks at the top
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+          // If hide completed is checked, filter tasks
+          return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
         return Tasks.find({}, { sort: { createdAt: -1 } });
       },
 });
