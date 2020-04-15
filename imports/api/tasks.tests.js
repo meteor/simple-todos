a05a065437858cb3c2f1cd14ab2e2456100d8664
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-04-14 22:27:46 -0500

    Step 11.4: Prepare the database for each test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,12 +1,28 @@
 /* eslint-env mocha */
 
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
-    describe('Tasks', () => {
-        describe('methods', () => {
-            it('can delete owned task', () => {
-            });
+  describe('Tasks', () => {
+    describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
         });
+      });
+
+      it('can delete owned task', () => {
+      });
     });
-} 
+  });
+}
